306ba44bfaeb51617fe706f9ee837b7621445d9b
agustin spataro <agustinspataro98@gmail.com>
2023-11-20 19:06:51 -0300

    proyect init

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    "text-[#000038]", // text purple
+    "text-[#0934B3]", // text purple 2
+    "text-[#3392FF]", // text purple 3
+    "text-[#55CBFF]",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    }),
+  ],
+};
+export default config;
